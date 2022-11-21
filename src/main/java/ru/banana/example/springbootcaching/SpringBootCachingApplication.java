11ae481bac7d367b206667db3e88b7c4ee8dcb4d
Pavel <P.A.Pisarev@yandex.ru>
2022-11-22 04:16:10 +0600

    init commit

---
+++ src/main/java/ru/banana/example/springbootcaching/SpringBootCachingApplication.java
@@ -0,0 +1,13 @@
+package ru.banana.example.springbootcaching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCachingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCachingApplication.class, args);
+	}
+
+}
